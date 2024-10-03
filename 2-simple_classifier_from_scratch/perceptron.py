a5955d18460c9e1416a858ff639c6554af5ecd48
yukiwuki <131858939+yukiwukii@users.noreply.github.com>
2024-10-04 00:49:31 +0800

    Added simple clasifier from scratch

---
+++ 2-simple_classifier_from_scratch/perceptron.py
@@ -0,0 +1,31 @@
+import numpy as np
+
+class Perceptron:
+    def __init__(self, eta = 0.01, n_iter = 50, random_state = 1):
+        self.eta = eta
+        self.n_iter = n_iter
+        self.random_state = random_state
+    
+    def fit(self, X, y):
+        rgen = np.random.RandomState(self.random_state)
+        self.w_ = rgen.normal(loc=0.0, scale=0.01,
+                              size=X.shape[1])
+        self.b_ = np.float(0.)
+        self.errors_ = []
+
+        for _ in range(self.n_iter):
+            errors = 0
+            for xi, target in zip(X, y):
+                update = self.eta * (target - self.predict(xi))
+                self.w_ += update * xi
+                self.b_ += update
+                errors += int(update != 0.0)
+            self.errors_.append(errors)
+        return self
+    
+    def net_input(self, X):
+        return np.dot(X, self.w_) + self.b_
+    
+    def predict(self, X):
+        return np.where(self.net_input(X) >= 0.0, 1, 0)
+    
---
+++ 2-simple_classifier_from_scratch/test.py
@@ -0,0 +1,17 @@
+class GFG:
+    
+    # methods
+    def add(self, a, b):
+        return a + b
+    def sub(self, a, b):
+        return a - b
+    
+print("test")
+
+class GFG2:
+    
+    # methods
+    def add(self, a, b):
+        return a + b
+    def sub(self, a, b):
+        return a - b
